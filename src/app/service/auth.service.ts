a33531db256feef94a5ef6419cd08c073edcd941
Reborsolli <renata.ferreirapn@gmail.com>
2021-10-02 19:49:53 -0300

    Inicio

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -3,6 +3,9 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { UserLogin } from '../Model/UserLogin';
 import { User } from '../Model/User';
+import { environment } from 'src/environments/environment.prod';
+
+
 
 @Injectable({
   providedIn: 'root'
@@ -21,6 +24,16 @@ export class AuthService {
   cadastrar( user: User ) : Observable<User> {
     return this.http.post<User>('https://blogrenata.herokuapp.com/usuarios/cadastrar', user)
 
-  
+  }
+
+  logado(){
+    let ok: boolean = false
+
+     if(environment.token != ''){
+       ok = true
+     }
+     
+    return ok
+
   }
 }
